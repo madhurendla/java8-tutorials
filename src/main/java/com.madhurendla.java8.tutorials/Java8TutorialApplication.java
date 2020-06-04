2db97a3c05e2f5627b659da26e732a91237ade4f
Madhu Rendla <mrendla@nisum.com>
2020-06-04 10:45:58 +0530

    initial setup for gradle

---
+++ src/main/java/com.madhurendla.java8.tutorials/Java8TutorialApplication.java
@@ -0,0 +1,12 @@
+package com.madhurendla.java8.tutorials;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8TutorialApplication{
+  public static void main(String[]args){
+
+    System.out.println("Hello world! Welcome to Java-8 !!");
+
+  }
+}
---
+++ src/test/java/com.madhurendla.java8.tutorials/Java8TutorialApplicationTest.java
@@ -0,0 +1,9 @@
+package com.madhurendla.java8.tutorials;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Java8TutorialApplicationTest{
+
+
+}
